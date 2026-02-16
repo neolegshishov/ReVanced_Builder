997227c24b931450bb434f462393fc5e6210a564
neolegshishov <143963760+neolegshishov@users.noreply.github.com>
2026-02-16 12:09:47 +0300

    Update YouTube.ps1

--- Scripts/YouTube.ps1
+++ Scripts/YouTube.ps1
@@ -1,13 +1,12 @@
 # Get the latest supported YouTube version to patch
-# https://api.revanced.app/docs/swagger
+# https://api.revanced.app
 $Parameters = @{
 	Uri             = "https://api.revanced.app/v4/patches/list"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
-
-$LatestSupportedYT = ((Invoke-RestMethod @Parameters) | Where-Object { $_.name -eq "Video ads" }).compatiblePackages.'com.google.android.youtube' | Sort-Object -Descending -Unique | Select-Object -First 1
-
+$Patches = (Invoke-RestMethod @Parameters | Where-Object -FilterScript {$_.name -eq "Video ads"})
+$LatestSupportedYT = $Patches.compatiblePackages."com.google.android.youtube" | Sort-Object -Descending -Unique | Select-Object -First 1
 $LatestSupported = $LatestSupportedYT.Replace(".", "-")
 
 # We need a NON-bundle version
@@ -28,7 +27,7 @@ $Request2 = Invoke-Webrequest @Parameters
 
 # Load AngleSharp
 Add-Type -Path "AngleSharp.dll"
-	  
+
 @($Request, $Request2) | ForEach-Object -Process {
 	$RequestVariable = $_
 
@@ -51,6 +50,7 @@ $DownloadURL = $Request.Links.href | Where-Object -FilterScript {$_ -match "down
 $Parameters = @{
 	Uri             = "https://www.apkmirror.com/$DownloadURL"
 	OutFile         = "ReVanced_Builder\youtube.apk"
+	UserAgent       = "Mozilla/5.0 (Linux; Android 13; itel A665L Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.7204.45 Mobile Safari/537.36"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
